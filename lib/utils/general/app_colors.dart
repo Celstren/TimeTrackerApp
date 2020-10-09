907385f7688c4e69805beb5e0498f235217ec281
Celstren <31768104+Celstren@users.noreply.github.com>
2020-10-09 09:53:43 -0500

    theme colors

--- lib/utils/general/app_colors.dart
+++ lib/utils/general/app_colors.dart
@@ -4,11 +4,16 @@ class AppColors {
   static const Color PrimaryWhite     = Color(0xFFFFFFFF); //#FFFFFF
   static const Color PrimaryBlack     = Color(0xFF000000); //#000000
   static const Color PrimaryDarkGrey  = Color(0xFFC0C0C0); //#C0C0C0
-  static const Color PrimaryGrey      = Color(0xFFECECEC); //#ECECEC
+  static const Color PrimaryGrey      = Color(0xFF666666); //#666666
 
-  static const Color PrimaryLightBlue = Color(0xFF89DAEE); //#89DAEE
-  static const Color PrimaryRed       = Color(0xFFFF0000); //#EE7C58
+  static const Color PrimaryLightBlue = Color(0xFF7EA6E0); //#7EA6E0
+  static const Color PrimaryBlue      = Color(0xFF4D7EA8); //#4D7EA8
+  static const Color PrimaryRed       = Color(0xFFA4303F); //#A4303F
   static const Color PrimaryOrange    = Color(0xFFEE7C58); //#EE7C58
   static const Color PrimaryYellow    = Color(0xFFEEC979); //#EEC979
-  static const Color PrimaryGreen     = Color(0xFF9EC084); //#9EC084
+  static const Color PrimaryGreen     = Color(0xFF63A375); //#63A375
+  
+  static const Color LinkBlue         = Color(0xFF0000CC); //#0000CC
+  static const Color IconBlue         = Color(0xFF0080F0); //#0080F0
+  static const Color DisableGrey      = Color(0xFFBFC2C9); //#BFC2C9
 }
